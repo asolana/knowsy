c04919f3721cb918fd64c95476e33f73fcc6fbb1
Mateo <mateo-536@hotmail.com>
2019-04-11 13:36:58 +0200

    adicion listadoTrabajosComponent, app.module

--- knowsy/src/app/app.module.ts
+++ knowsy/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TrabajosComponent } from './trabajos/trabajos.component';
+import { ListadoTrabajosComponent } from './listado-trabajos/listado-trabajos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TrabajosComponent
+    TrabajosComponent,
+    ListadoTrabajosComponent
   ],
   imports: [
     BrowserModule,
