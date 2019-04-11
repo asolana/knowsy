f46c1842a7f6bbe7b2c47603696c7af291600c64
joseprigo <jrrr13@gmail.com>
2019-04-11 15:06:18 +0200

    lista trabajo conectada al servicio

--- knowsy/src/app/listado-trabajos/listado-trabajos.component.ts
+++ knowsy/src/app/listado-trabajos/listado-trabajos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Trabajo } from '../modelo/trabajo';
+import { TrabajosService } from '../servicio/trabajos.service';
 
 @Component({
   selector: 'listado-trabajos',
@@ -8,17 +10,23 @@ import { Component, OnInit } from '@angular/core';
 export class ListadoTrabajosComponent implements OnInit {
 
   trabajos = [
-    {id:1, nombre: "pasear perro", idusuario:1, puntuacion: 4, descripcion: "paseo perros", precio: 450, img:"assets/img/foto01.jpg"},
-    {id:2, nombre: "cocinar pato", idusuario:1, puntuacion: 4, descripcion: "delicioso", precio: 420, img:"assets/img/foto02.jpg"},
-    {id:3, nombre: "reparar goteras", idusuario:2, puntuacion: 4, descripcion: "no goteará ", precio: 50, img:"assets/img/foto03.jpg"},
-    {id:4, nombre: "peluquería", idusuario:1, puntuacion: 4, descripcion: "lorem ipsum", precio: 650, img:"assets/img/foto04.jpg"},
-    {id:5, nombre: "bañar caballos", idusuario:1, puntuacion: 4, descripcion: "lorem ipsum", precio: 450, img:"assets/img/foto05.jpg"},
-    {id:6, nombre: "programación ", idusuario:1, puntuacion: 4, descripcion: "programación entre comillas, real", precio: 450, img:"assets/img/foto06.jpg"},
+    {id:1, nombre: "pasear perro", idusuario:1, puntuacion: 4, descripcion: "paseo perros", precio: 450, img:"img/src/foto01.jpg"},
+    {id:2, nombre: "cocinar pato", idusuario:1, puntuacion: 4, descripcion: "delicioso", precio: 420, img:"img/src/foto02.jpg"},
+    {id:3, nombre: "reparar goteras", idusuario:2, puntuacion: 4, descripcion: "no goteará ", precio: 50, img:"img/src/foto03.jpg"},
+    {id:4, nombre: "peluquería", idusuario:1, puntuacion: 4, descripcion: "lorem ipsum", precio: 650, img:"img/src/foto04.jpg"},
+    {id:5, nombre: "bañar caballos", idusuario:1, puntuacion: 4, descripcion: "lorem ipsum", precio: 450, img:"img/src/foto05.jpg"},
+    {id:6, nombre: "programación ", idusuario:1, puntuacion: 4, descripcion: "programación entre comillas, real", precio: 450, img:"img/src/foto06.jpg"},
     ];
 
-  constructor() { }
+  constructor(private _traServ: TrabajosService) { }
 
   ngOnInit() {
+    //trabajos = this._traServ.getTrabajos();
+
+    this._traServ.getTrabajos().subscribe((datos) => {
+      //console.log('datos:', listaTareas);
+      this.trabajos = datos;
+    });
   }
 
 }
