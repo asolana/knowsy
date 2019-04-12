7ce83a5b9410f34f01ab7f2735d9b044288b5d7f
joseprigo <jrrr13@gmail.com>
2019-04-12 10:13:21 +0200

    nueva clase Usuario

---
+++ knowsy/src/app/modelo/usuario.ts
@@ -0,0 +1,11 @@
+export class Usuario{
+    
+    constructor(public id:number,
+        public nombre:string,
+        public descripcion:string,
+        public img:string,
+        public estado:boolean,
+        public tokens:number){
+    }
+
+}
