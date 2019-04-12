175dccd85453e6807920f7f911d3f3f55db2c94c
carlasarango <sarangocarla12@gmail.com>
2019-04-12 10:29:51 +0200

    cosas varias del trabajodetalle

--- knowsy/src/app/app-routing.module.ts
+++ knowsy/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TrabajosComponent } from './trabajos/trabajos.component';
+import { TrabajodetalleComponent } from './trabajos/trabajodetalle/trabajodetalle.component';
 
 const routes: Routes = [
   {path: 'trabajos', component: TrabajosComponent, pathMatch: 'full'},
+  {path: 'trabajos/:id', component: TrabajodetalleComponent, pathMatch: 'full'},
   {path: '', redirectTo:'trabajos', pathMatch: 'full'}
 ];
 
--- knowsy/src/app/trabajos/trabajodetalle/trabajodetalle.component.ts
+++ knowsy/src/app/trabajos/trabajodetalle/trabajodetalle.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-trabajodetalle',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrabajodetalleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _http:HttpClient) { }
 
   ngOnInit() {
   }
