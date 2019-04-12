27490140eef50d5ee4647419e9293ed0eebf3ef5
Bit Aitor <a.solana23@gmail.com>
2019-04-12 09:39:27 +0200

    Actualizacion filtro servicio trabajos

--- knowsy/src/app/servicio/trabajos.service.ts
+++ knowsy/src/app/servicio/trabajos.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError, of } from 'rxjs';
+import { Observable, throwError, of, BehaviorSubject } from 'rxjs';
 import { Trabajo } from '../modelo/trabajo';
-import { tap,catchError } from 'rxjs/operators';
+import { tap, catchError } from 'rxjs/operators';
 
 
 
@@ -10,82 +10,52 @@ import { tap,catchError } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class TrabajosService {
-
   //Array para la lista de trabajos
   private _trabajos: Trabajo[] = null;
   //Observable de la lista de trabajos
   private _trabajosObs: Observable<Trabajo[]>;
+  private $tareasSub = new BehaviorSubject(this._trabajos);
 
   //Constructor con el cliente para peticiones get
-  constructor(private _http:HttpClient) { }
+  constructor(private _http: HttpClient) { }
 
-  //Peticion de datos al servidor
-  getTrabajosFromAPIEmpty(){
-    if(!this._trabajosObs){
-      this._trabajosObs = this._http.get<Trabajo[]>('http://www.mocky.io/v2/5caf02663400009324ab6fa6').pipe(
-        tap(trabs => this._trabajos = trabs),
-        catchError(error => {
-          console.log("Error:", error);
-          return throwError(error);
-        })
-      );
-    }
-  }
-  getTrabajosFromAPI(id): Observable<Trabajo[]> {
-    console.log("va:", this._trabajos);
+  getTrabajosFromAPI(): Observable<Trabajo[]> {
 
-    if(!this._trabajos){
-      this.getTrabajosFromAPIEmpty();
-      return this._trabajosObs;
-    }else{  
-      //Aqui me he quedado
-      console.log("va3");
-      this._trabajosObs = this._http.get<Trabajo[]>('http://www.mocky.io/v2/5caf02663400009324ab6fa6').pipe(
-       tap(trabs => {
-         //filtro
-        //  this._trabajos = trabs
-        trabs.find(function(tra){
-          
-           return tra.nombre == id;
-         })
-        }),
-       catchError(error => {
-         console.log("Error:", error);
-         return throwError(error);
-       })
-     );
-    }
-  //  if (this._trabajos) {
-  //    return of(this._trabajos);
-  //  } else if (this._trabajosObs) {
-  //     this._trabajosObs.subscribe(function(lista){
-  //       console.log("trabajos obs", lista);
-  //    });
-  //    return this._trabajosObs;
-  //  } else {
-  //    this._trabajosObs = this._http.get<Trabajo[]>('http://www.mocky.io/v2/5caf02663400009324ab6fa6').pipe(
-  //      tap(trabs => {
-  //        //filtro
-  //       //  this._trabajos = trabs
-  //       trabs.find(function(tra){
-  //          return tra.nombre == id;
-  //        })
-  //       }),
-  //      catchError(error => {
-  //        console.log("Error:", error);
-  //        return throwError(error);
-  //      })
-  //    );
-  //    return this._trabajosObs;
-  //  }
+    this._trabajosObs = this.$tareasSub.asObservable();
+    this._http.get<Trabajo[]>('http://www.mocky.io/v2/5caf02663400009324ab6fa6').subscribe(
+      data => {
+        this._trabajos = data;
+        this.$tareasSub.next(this._trabajos);
+      },
+      error => {
+        console.log("Error:", error);
+        return throwError(error);
+      }
+    );
+
+    return this._trabajosObs;
+
   }
-  
+
+  buscarTrabajos(busqueda: string):void {
+    this._http.get<Trabajo[]>('http://www.mocky.io/v2/5caf02663400009324ab6fa6').subscribe(
+      data => {
+        this._trabajos = data.filter( unT=> unT.nombre.toLowerCase().indexOf(busqueda.toLowerCase())>=0 );
+        this.$tareasSub.next(this._trabajos);
+      },
+      error => {
+        console.log("Error:", error);
+        return throwError(error);
+      }
+    );
+  }
+
   //Funcion que devuelve un array de trabajos
   getTrabajos(): Trabajo[] { return this._trabajos; }
 
   //Funcion que a la cual se le pasa un id y devuelve el trabajo con ese id
   getTrabajoById(id): Trabajo {
-    return this._trabajos.find(function(trabajo){
+    return this._trabajos.find(function (trabajo) {
       return trabajo.id == id;
     });
   }
