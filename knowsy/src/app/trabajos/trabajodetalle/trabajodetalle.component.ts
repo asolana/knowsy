ed247b2efae8a8e5d16cb7572efcc87edf6476bf
carlasarango <sarangocarla12@gmail.com>
2019-04-12 15:29:27 +0200

    trabajodetalle!!

--- knowsy/src/app/trabajos/trabajodetalle/trabajodetalle.component.ts
+++ knowsy/src/app/trabajos/trabajodetalle/trabajodetalle.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
 import { TrabajosService } from 'src/app/servicio/trabajos.service';
+import { ActivatedRoute } from '@angular/router';
+import { Trabajo } from 'src/app/modelo/trabajo';
+
 
 @Component({
   selector: 'trabajodetalle',
@@ -8,11 +10,33 @@ import { TrabajosService } from 'src/app/servicio/trabajos.service';
   styleUrls: ['./trabajodetalle.component.scss']
 })
 export class TrabajodetalleComponent implements OnInit {
-  public trabajo= null;
+  clickMessage='';
   
-  constructor(private _trabajoServ:TrabajosService) { }
+
+  trabajo: Trabajo;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _trabajoServ: TrabajosService,
+  ) { }
 
   ngOnInit() {
+    this._route.params.subscribe(parametros => {
+      console.log('paramentros:', parametros);
+      const id = parametros['id'];
+      this._trabajoServ.getTrabajoById(id).subscribe(trab => {
+        this.trabajo = trab;
+      });
+      console.log('trabajo:', this.trabajo);
+      
+    })
+  
+  
   }
-
+  contratar(){
+    this.clickMessage='FELICIDADES!';
+    alert(this.clickMessage);
+    
+  }
+ 
 }
