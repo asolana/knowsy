2eb9b01bdb99e7813b3aeeaf5ca44af20f2b5ea0
joseprigo <jrrr13@gmail.com>
2019-04-12 11:23:17 +0200

    unión lista-trabajos

--- knowsy/src/app/usuario/usuario.component.ts
+++ knowsy/src/app/usuario/usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../modelo/usuario';
 
 @Component({
   selector: 'app-usuario',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuario.component.scss']
 })
 export class UsuarioComponent implements OnInit {
+
+  usuario = new Usuario(1,"admin ","soy un administrador","assets/img/usuario/admin.jpg",true,23000);
 
   constructor() { }
 
