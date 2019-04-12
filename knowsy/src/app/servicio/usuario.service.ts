dd3a9d15c44233bfd8639bc4f9712f2a020958cf
Bit Aitor <a.solana23@gmail.com>
2019-04-12 12:57:30 +0200

    Creacion usuario service

--- knowsy/src/app/servicio/usuario.service.ts
+++ knowsy/src/app/servicio/usuario.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Usuario } from '../modelo/usuario';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor() { }
+  private _usuarios: Usuario[] = null;
+
+  constructor(private _http:HttpClient) { }
+
+  getUsuariosFromAPI(): Observable<Usuario[]> {
+      return this._http.get<Usuario[]>('http://www.mocky.io/v2/5cb0477b3100004b00e132ff');
+  } 
+
+  getUsuarioById(id){
+    this.getUsuariosFromAPI().subscribe((usuarios) => { this._usuarios = usuarios});
+    return this._usuarios.find(function (unUser) {
+      return unUser.id == id;
+   });
+  }
 }
