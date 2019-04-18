16d5d148ee88f2fed32cf5df3641c58a66a2f332
Bit Aitor <a.solana23@gmail.com>
2019-04-18 14:58:31 +0200

    Actualizacion Valoraciones

--- knowsy/src/app/trabajos/valoracion/valoracion.component.ts
+++ knowsy/src/app/trabajos/valoracion/valoracion.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TrabajosService } from 'src/app/servicio/trabajos.service';
+import { UsuarioService } from 'src/app/servicio/usuario.service';
+import { ActivatedRoute } from '@angular/router';
+import { Trabajo } from 'src/app/modelo/trabajo';
 
 @Component({
   selector: 'valoracion',
@@ -7,11 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ValoracionComponent implements OnInit {
 
-puntuacion = 1;
+  trabajo: Trabajo;
+  puntuacion = 1;
    
-  constructor() { }
+  constructor(
+    private _trabServ:TrabajosService, 
+    private _userServ:UsuarioService,
+    private _route:ActivatedRoute) { }
 
   ngOnInit() {
+    this._route.params.subscribe(parametros => {
+      console.log('paramentros:', parametros);
+      const id = parametros['id'];
+      this._trabServ.getTrabajoById(id).subscribe(trab => {
+        this.trabajo = trab;
+      });
+      console.log('trabajo:', this.trabajo);
+      
+    })
+  }
+
+
+  reportar(){
+
+  }
+
+  valorar(){
+
   }
 
 }
