6f732f1b9d772688d543cf67ee9a32dc4cbf944c
singularAna <gruptrabajo1@gmail.com>
2019-04-18 14:20:33 +0200

    valoración

--- knowsy/src/app/app-routing.module.ts
+++ knowsy/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { TrabajosComponent } from './trabajos/trabajos.component';
 import { TrabajodetalleComponent } from './trabajos/trabajodetalle/trabajodetalle.component';
 import { UsuarioComponent } from './usuario/usuario.component';
+import { ValoracionComponent } from './trabajos/valoracion/valoracion.component';
 
 const routes: Routes = [
   {path: 'trabajos', component: TrabajosComponent, pathMatch: 'full'},
   {path: 'trabajos/:id', component: TrabajodetalleComponent, pathMatch: 'full'},
   {path: 'usuario/:id', component: UsuarioComponent, pathMatch: 'full'},
+ 
+  {path: 'valoracion', component: ValoracionComponent, pathMatch: 'full'},
+
   {path: '', redirectTo:'trabajos', pathMatch: 'full'},
   { path: '**', component: TrabajosComponent, pathMatch: 'full' },
 ];
