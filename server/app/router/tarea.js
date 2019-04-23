db3f0a759b873536768231ffca4509c558f0dac4
Bit Aitor <a.solana23@gmail.com>
2019-04-23 10:42:30 +0200

    tareas get

---
+++ server/app/router/tarea.js
@@ -0,0 +1,116 @@
+const router = require('express').Router();
+const Tarea = require('../models/tarea');
+
+router.route('/tareas')
+
+    .get(function (req, res) {
+        Tarea.find().then(tareas => {
+            res.json(tareas);
+        }).catch(err => {
+            console.log('Error getting tareas:', err);
+            res.status(500).send({ message: 'Server error' });
+        });
+
+    })
+    .post(function (req, res) {
+        let tarea = new Tarea();
+
+        tarea.nombre = req.body.nombre;
+        tarea.idusuario = req.body.idusuario;
+        tarea.puntuacion = req.body.puntuacion;
+        tarea.img = req.body.img;
+        tarea.contpuntuacion = req.body.contpuntuacion;
+        tarea.descripcion = req.body.descripcion;
+        tarea.precio = req.body.precio;
+        tarea.idcategoria = req.body.idcategoria;
+
+        tarea.save().then(tareaGuardada => {
+            console.log('tareaGuardada:', tareaGuardada);
+
+            if (tareaGuardada) {
+                res.json(tareaGuardada);
+            }
+        }).catch(err => {
+            console.log('Error saving new tarea:', err);
+            res.status(500).send({ message: 'Server error' });
+        });
+
+    });
+
+router.route('/tareas/:id')
+
+    .get(function (req, res) {
+
+        Tarea.findById(req.params.id).then(tarea => {
+            res.json(tarea);
+        }).catch(err => {
+            console.log('Error getting tarea:', err);
+            res.status(500).send({ message: 'Server error' });
+        });
+
+    })
+    .put(function (req, res) {
+        let tarea = new Tarea();
+
+        tarea.nombre = req.body.nombre;
+        tarea.idusuario = req.body.idusuario;
+        tarea.puntuacion = req.body.puntuacion;
+        tarea.img = req.body.img;
+        tarea.contpuntuacion = req.body.contpuntuacion;
+        tarea.descripcion = req.body.descripcion;
+        tarea.precio = req.body.precio;
+        tarea.idcategoria = req.body.idcategoria;
+
+        Usuario.findById(req.params.id).then(unaTarea => {
+            if (unaTarea) {
+                res.status(409).send({ message: 'This email already exists' });
+                unaTarea = null;
+            } else { unaTarea = usuario; }
+
+            return unaTarea;
+        }).then(unaTarea => {
+            if (unaTarea) unaTarea.save();
+
+            return unaTarea;
+        }).then(usuarioGuardado => {
+            console.log('usuarioGuardado:', usuarioGuardado);
+
+            if (usuarioGuardado) {
+                res.json(usuarioGuardado);
+            }
+        }).catch(err => {
+            console.log('Error saving new usuario:', err);
+            res.status(500).send({ message: 'Server error' });
+        });
+
+
+    });
+
+
+
+
+
+
+
+
+
+
+function generatePass() {
+    let password = "";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let possibleletters = "BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz";
+    let possiblevowels = "AEIOUYaeiouy";
+    let possiblenumbers = "1234567890";
+
+    for (let i = 0; i < 6; i++) {
+        if ((i + 1) % 2) password += possibleletters.charAt(Math.floor(Math.random() * possibleletters.length));
+        else password += possiblevowels.charAt(Math.floor(Math.random() * possiblevowels.length));
+    }
+
+    password += possiblenumbers.charAt(Math.floor(Math.random() * possiblenumbers.length));
+    password += possiblenumbers.charAt(Math.floor(Math.random() * possiblenumbers.length));
+
+    return password;
+}
+
+module.exports = router;
